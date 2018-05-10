28e751a8e7ee15a365aa51ead04bf7b6879ce8d8
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-05-09 21:30:34 -0400

    get parser script to the point where it displays real data

--- parser.py
+++ parser.py
@@ -1,20 +1,47 @@
-inputname = 'human-freedom-index.csv.13'
+from matplotlib import pyplot
+inputname = '../human-freedom-index.csv.13'
 
 page = open(inputname, 'r')
 
-strained_data = []
+split_data = []
 
 for line in page:
     if '#' != line[0]:
-        stripped_line = line[:-8]
-        split_line = stripped_line.split(',')
+        split_line = line[:-9].split(',')
+        split_data.append(split_line)
+
+
+filtered_data = filter(lambda x: len(x) > 3, split_data)
+
+x_points = []
+y_points = []
+counter = 0
+
+for point  in filtered_data:
+    print(point)
+    if counter == 0:
+        counter += 1
+        continue
+    try:
+        floated_x = (float)(point[2])
+        floated_y = (float)(point[3])
+    except:
+        continue
+    x_points.append(floated_x)
+    y_points.append(floated_y)
+
+
+print(x_points)
+print(y_points)
+
+pyplot.scatter(x_points, y_points, s=1, c='blue')
 
 
 
-line_list = []
+#x_points = [x for x in range(10)]
 
-for line in strained_data:
-    if '' != 
+#y_points = [x*2 for x in range(10)]
 
+#pyplot.plot(x_points, y_points, color='green', linestyle='solid')
 
-print(strained_data)
+pyplot.show()
